278e2cc4f59ef4352046f9ec0783e2a69200c3d1
Bundi-py <58522757+Bundi-py@users.noreply.github.com>
2020-05-07 09:41:25 +0630

    commit 1

--- dict_v2.py
+++ dict_v2.py
@@ -1,160 +1,207 @@
 import csv
 from pathlib import Path
 
-
-path = Path.cwd() / 'dict_ps.csv'
-
-with open(path, newline='', encoding='utf-8') as csvfile_ps:
+path_ps = Path.cwd() / 'dict_ps.csv'
+with open(path_ps, newline='', encoding='utf-8') as csvfile_ps:
     reader_ps = csv.reader(csvfile_ps)
     mydict_ps = {rows[0]: rows[1] for rows in reader_ps}
 
+path_sp = Path.cwd() / 'dict_sp.csv'
+with open(path_sp, newline='', encoding='utf-8') as csvfile_sp:
+    reader_sp = csv.reader(csvfile_sp)
+    mydict_sp = {rows[0]: rows[1] for rows in reader_sp}
+
+
+def mainMenu():
+    print('''Welcome to the CodingNomads Dictionary:
+            1. Pali-Serbian
+            2. Serbian-Pali
+            3. Quit''')
+
+    choice = input('Enter choice: ')
+    if choice == '1':
+        subMenu_ps()
+    elif choice == '2':
+        subMenu_sp()
+    elif choice == '3':
+        exit
+    else:
+        print('\nInvalid choice. Enter 1-3')
+        mainMenu()
+
+
+def subMenu_ps():
+    print()
+    print()
+    print('''Welcome to the Pali-Serbian Dictionary:
+            1. Search word
+            2. Add word
+            3. Change word
+            4. List words starting with letters entered
+            5. Back''')
+
+    choice = input('Enter choice: ')
+    if choice == '1':
+        search_p = input('Enter search word: ')
+        searching_p(search_p)
+        subMenu_ps()
+    elif choice == '2':
+        add_pali = input('Enter Pali word: ')
+        add_serbian = input('Enter Serbian word: ')
+        adding_p(add_pali, add_serbian)
+        subMenu_ps()
+    elif choice == '3':
+        error = input('Enter a wrong word: ')
+        correction = input('Enter a correct word: ')
+        correcting_p(error, correction)
+        subMenu_ps()
+    elif choice == '4':
+        letter_ps = input('Enter inital letters to list the words: ')
+        listing_ps(letter_ps)
+        subMenu_ps()
+    elif choice == '5':
+        mainMenu()
+    else:
+        print('\nInvalid choice. Enter 1-5')
+        subMenu_ps()
 
 # Search a Pali word in the Dictionary
 
 
-def searching_p(search):
-    print('This Pali word in Serbian means: ', mydict_ps[search])
+def searching_p(search_p):
+    if search_p in mydict_ps:
+        print('This Pali word in Serbian means: ', mydict_ps[search_p])
+    else:
+        print('This word is not in a dictionary')
     return
 
 # Adding a new Pali word to the Dictionary
 
 
 def adding_p(add_pali, add_serbian):
-    mydict[add_pali] = add_serbian
+    mydict_ps[add_pali] = add_serbian
     print('Done!')
 
     # Writing new status of the Dictionary
-    with open(path, mode='w', encoding='utf-8', newline='') as outfile:
+    with open(path_ps, mode='w', encoding='utf-8', newline='') as outfile:
         writer = csv.writer(outfile)
         for i in mydict_ps.items():
             writer.writerow(i)
 
     print("Writing complete")
 
+
 # Correcting a Pali word in the Dictionary
-
-
 def correcting_p(error, correction):
     x = mydict_ps.get(error)
     if x:
-        mydict[correction] = mydict_ps[error]
+        mydict_ps[correction] = mydict_ps[error]
         del mydict_ps[error]
         print(f'{error} replaced with {correction}')
     else:
         print(f'{error} is not in the dictionary')
 
     # Writing new status of the Dictionary
-    with open(path, mode='w', encoding='utf-8', newline='') as outfile:
+    with open(path_ps, mode='w', encoding='utf-8', newline='') as outfile:
         writer = csv.writer(outfile)
         for i in mydict_ps.items():
             writer.writerow(i)
-
     print("Writing complete")
 
 
+# Listing words starting with letters
+def listing_ps(letter_ps):
+    for k, v in mydict_ps.items():
+        if k.startswith(letter_ps):
+            print(k, ' = ', v)
+
 # Serbian-Pali
 
-path = Path.cwd() / 'dict_sp.csv'
 
-with open(path, newline='', encoding='utf-8') as csvfile_sp:
-    reader_sp = csv.reader(csvfile_sp)
-    mydict_sp = {rows[0]: rows[1] for rows in reader_sp}
+def subMenu_sp():
+    print('''Welcome to the Serbian-Pali Dictionary:
+            1. Search word
+            2. Add word
+            3. Change word
+            4. List words starting with letters entered
+            5. Back''')
+
+    choice = input('Enter choice: ')
+    if choice == '1':
+        search_s = input('Enter search word: ')
+        searching_s(search_s)
+        subMenu_sp()
+    elif choice == '2':
+        add_serbian = input('Enter Serbian word: ')
+        add_pali = input('Enter Pali word: ')
+        adding_p(add_serbian, add_pali)
+        subMenu_sp()
+    elif choice == '3':
+        error = input('Enter wrong word: ')
+        correction = input('Enter correct word: ')
+        correcting_s(error, correction)
+        subMenu_sp()
+    elif choice == '4':
+        letter_sp = input('Enter inital letters to list the words: ')
+        listing_sp(letter_sp)
+        subMenu_sp()
+    elif choice == '5':
+        mainMenu()
+    else:
+        print('\nInvalid choice. Enter 1-4')
+        mainMenu()
 
 # Search a Serbian word in the Dictionary
 
 
-def searching_s(search):
-    print('This Pali word in Serbian means: ', mydict_sp[search])
+def searching_s(search_s):
+    if search_s in mydict_sp:
+        print('This Pali word in Serbian means: ', mydict_sp[search_s])
+    else:
+        print('This word is not in a dictionary')
     return
 
 # Adding a new Serbian word to the Dictionary
 
 
-def adding_s(add_pali, add_serbian):
-    mydict_sp[add_pali] = add_serbian
+def adding_s(add_serbian, add_pali):
+    mydict_sp[add_serbian] = add_pali
     print('Done!')
 
     # Writing new status of the Dictionary
-    with open(path, mode='w', encoding='utf-8', newline='') as outfile:
+    with open(path_sp, mode='w', encoding='utf-8', newline='') as outfile:
         writer = csv.writer(outfile)
         for i in mydict_sp.items():
             writer.writerow(i)
 
     print("Writing complete")
 
+
 # Correcting a Serbian word in the Dictionary
-
-
 def correcting_s(error, correction):
     x = mydict_sp.get(error)
     if x:
-        mydict[correction] = mydict_sp[error]
+        mydict_sp[correction] = mydict_sp[error]
         del mydict_sp[error]
         print(f'{error} replaced with {correction}')
     else:
         print(f'{error} is not in the dictionary')
 
     # Writing new status of the Dictionary
-    with open(path, mode='w', encoding='utf-8', newline='') as outfile:
+    with open(path_sp, mode='w', encoding='utf-8', newline='') as outfile:
         writer = csv.writer(outfile)
         for i in mydict_sp.items():
             writer.writerow(i)
-
     print("Writing complete")
 
+# Listing words starting with letters
 
-mans = True
-ans = ''
-while mans:
-    print('''
-    a. Pali-Serbian Dictionary
-    b. Serbian-Pali Dictionary
-    c. Quit
-    ''')
-    mans = input('What dictionary would you like to retrieve? ')
-    if mans == '1':
-        print("""
-            1. Look up Pali word
-            2. Add a Pali word
-            3. Correct a Pali word
-            4. Exit/Quit
-            """)
-        ans = input("What would you like to do? ")
-        if ans == "1":
-            searching_p(input('Search for Pali word: '))
-        elif ans == "2":
-            adding_p(input('To add a new Pali word, type it here: '),
-                     input('Now type its meaning in Serbian: '))
-        elif ans == "3":
-            correcting_p(input('Type Pali word to be corrected: '),
-                         input('Type correct version of the Pali word: '))
-        elif ans == "4":
-            print("\n Goodbye")
-            break
-        elif ans != "":
-            print("\n Not Valid Choice! Try again")
-    elif mans == '2':
-        print("""
-            1. Look up Serbian word
-            2. Add a Serbian word
-            3. Correct a Serbian word
-            4. Exit/Quit
-            """)
-        ans = input("What would you like to do? ")
-        if ans == "1":
-            searching_s(input('Search for Serbian word: '))
-        elif ans == "2":
-            adding_s(input('To add a new Serbian word, type it here: '),
-                     input('Now type its meaning in Pali: '))
-        elif ans == "3":
-            correcting_s(input('Type Serbian word to be corrected: '),
-                         input('Type correct version of the Serbian word: '))
-        elif ans == "4":
-            print("\n Goodbye")
-            break
-        elif ans != "":
-            print("\n Not Valid Choice! Try again")
-    else:
-        break
 
-print('Good bye!')
+def listing_sp(letter_sp):
+    for k, v in mydict_sp.items():
+        if k.startswith(letter_sp):
+            print(k, ' = ', v)
+
+
+# main routine
+mainMenu()
